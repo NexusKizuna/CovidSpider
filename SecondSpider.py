a982414a250b712adea27b8cf7f86ae6e2647d15
NexusKizuna <1430438799@qq.com>
2021-05-17 16:10:56 +0800

    new spider

--- SecondSpider.py
+++ SecondSpider.py
@@ -8,7 +8,10 @@ class SecondSpider:
     def __init__(self):
         self.url = 'https://api.inews.qq.com/newsqa/v1/query/pubished/daily/list?province='
         self.lis = []
-        self.provinceName = []
+        self.province = ['新疆', '黑龙江', '吉林', '辽宁', '河北', '天津', '内蒙古', '北京',
+                         '山东', '山西', '陕西', '宁夏', '甘肃', '青海', '西藏', '江苏', '河南',
+                         '浙江', '上海', '安徽', '湖北', '重庆', '四川', '贵州', '湖南', '江西',
+                         '福建', '广东', '广西', '云南', '台湾', '海南', '澳门', '香港']
 
     def start(self):
         pass
