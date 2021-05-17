caa8fc8e16e157f2f08aebb70717a47f83c4d6e5
NexusKizuna <1430438799@qq.com>
2021-05-17 15:33:15 +0800

    initial commit

--- MajorGui.py
+++ MajorGui.py
@@ -1,7 +1,26 @@
-from kivy.uix import button
+from kivy.uix.button import Button
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
 
-class LogicalLayout(BoxLayout):
+class LogicalLayout(GridLayout):
     def __init__(self):
-        BoxLayout.__init__(self, row=4, column=4)
+        GridLayout.__init__(self, cols=4, spacing=30, padding=30)
+        for i in range(5):
+            self.add_widget(Label(text=''))
+        self.button1 = Button(text='touch me')
+        self.add_widget(self.button1)
+        self.button2 = Button(text='don\'t touch me')
+        self.add_widget(self.button2)
+        for i in range(5):
+            self.add_widget(Label(text=''))
+
+
+class MyApp(App):
+    def build(self):
+        return LogicalLayout()
+
+
+if __name__ == '__main__':
+    app = MyApp()
+    app.run()
